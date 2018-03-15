bb2c56a0c133be56197fa89d7a2ed9b54d4c1c05
Ryan Dabler <rpdabler@gmail.com>
2018-03-15 11:59:33 -0600

    create middleware to check if mongo doc exists

---
+++ src/middleware/does-not-exist.js
@@ -0,0 +1,18 @@
+function doesNotExist(model, field) {
+    return (req, res, next) => {
+        const queryDoc = {};
+        queryDoc[field] = req.body[field];
+
+        return model.find(queryDoc)
+            .count()
+            .then(count => {
+                if (count) {
+                    next( { status: 422, message: `${field} already exists` } );
+                } else {
+                    next();
+                }
+            });
+    }
+}
+
+module.exports = { doesNotExist };
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,11 @@
 const { checkRequiredFields } = require("check-required-fields");
 const { checkFieldType }      = require("check-field-type");
 const { validateWhitespace }  = require("validate-whitespace");
+const { doesNotExist }        = require("does-not-exist");
 
 module.exports = {
     checkRequiredFields,
     checkFieldType,
-    validateWhitespace
+    validateWhitespace,
+    doesNotExist
 };
