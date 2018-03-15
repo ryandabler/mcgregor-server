701e4ddb596522c1d1b7f639e2d4fd329e0c4658
Ryan Dabler <rpdabler@gmail.com>
2018-03-15 11:06:09 -0600

    add user email

--- src/models/users.js
+++ src/models/users.js
@@ -6,6 +6,7 @@ const bcrypt   = require("bcrypt");
 // Schema
 const userSchema = mongoose.Schema({
     username: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, required: true }
 });
 
