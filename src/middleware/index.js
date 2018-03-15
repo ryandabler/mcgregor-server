b9071e1829731ea59a46998aa72297b5653b8e17
Ryan Dabler <rpdabler@gmail.com>
2018-03-15 13:14:46 -0600

    fixed paths for middleware requires

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,7 @@
-const { checkRequiredFields } = require("check-required-fields");
-const { checkFieldType }      = require("check-field-type");
-const { validateWhitespace }  = require("validate-whitespace");
-const { doesNotExist }        = require("does-not-exist");
+const { checkRequiredFields } = require("./check-required-fields");
+const { checkFieldType }      = require("./check-field-type");
+const { validateWhitespace }  = require("./validate-whitespace");
+const { doesNotExist }        = require("./does-not-exist");
 
 module.exports = {
     checkRequiredFields,
