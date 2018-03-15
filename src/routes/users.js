b328592343846513558f6fe065bb97e04230b217
Ryan Dabler <rpdabler@gmail.com>
2018-03-15 13:55:11 -0600

    create user router

---
+++ src/routes/users.js
@@ -0,0 +1,42 @@
+"use strict";
+
+////////////////////////////
+// Initialize
+////////////////////////////
+const router     = require("express").Router();
+const jsonParser = require("body-parser").json();
+const { User }   = require("../models");
+const {
+    checkRequiredFields,
+    checkFieldType,
+    validateWhitespace,
+    ensureDoesNotExist
+} = require("../middleware");
+
+////////////////////////////
+// Routes
+////////////////////////////
+router.post(
+    "/",
+    jsonParser,
+    checkRequiredFields(["username", "email", "password"]),
+    checkFieldType(["username", "password"], ["string", "string"]),
+    validateWhitespace(["username", "password"]),
+    ensureDoesNotExist(User, "username"),
+    (req, res) => {
+        let { username, password, email } = req.body;
+        email = email.trim();
+
+        return User.hashPassword(password)
+            .then(hash => {
+                return User.create({
+                    username,
+                    email,
+                    password: hash
+                });
+            })
+            .then(user => res.status(201).json(user.serialize()))
+            .catch(err => res.status(500).json( { message: "Internal server error" } ));
+});
+
+module.exports = { router };
