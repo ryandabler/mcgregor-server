f6f424951b23ce8b47993c74f7df3ea1253c03e7
Ryan Dabler <rpdabler@gmail.com>
2018-03-16 10:11:19 -0600

    create validateIds middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,10 +2,12 @@ const { checkRequiredFields } = require("./check-required-fields");
 const { checkFieldType }      = require("./check-field-type");
 const { validateWhitespace }  = require("./validate-whitespace");
 const { ensureDoesNotExist }  = require("./ensure-does-not-exist");
+const { validateIds }         = require("./validate-ids");
 
 module.exports = {
     checkRequiredFields,
     checkFieldType,
     validateWhitespace,
-    ensureDoesNotExist
+    ensureDoesNotExist,
+    validateIds
 };
