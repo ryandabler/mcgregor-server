1de8184784592de13ca0b9e4ad29a72042b11632
Ryan Dabler <rpdabler@gmail.com>
2018-03-16 09:31:11 -0600

    create crop model

---
+++ src/models/crop.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+// Schema
+const cropSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    variety: { type: String, required: true },
+    plant_date: { type: Date, required: true },
+    germination_days: { type: Number, required: true },
+    harvest_days: { type: Number, required: true },
+    planting_depth: Number,
+    row_spacing: Number,
+    seed_spacing: Number,
+    garden: { type: mongoose.Schema.Types.ObjectId, required: true }
+});
+
+// Instance methods
+cropSchema.methods.serialize = function() {
+    return {
+        name: this.name,
+        variety: this.variety,
+        plant_date: this.plant_date,
+        germination_days: this.germination_days,
+        harvest_days: this.harvest_days,
+        planting_depth: this.planting_depth,
+        row_spacing: this.row_spacing,
+        seed_spacing: this.seed_spacing
+    }
+}
+
+// Model
+const Crop = mongoose.model("Crop", cropSchema);
+
+module.exports = { Crop };
--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 const { User } = require("./user");
+const { Crop } = require("./crop");
 
-module.exports = { User };
+module.exports = { User, Crop };
