6fbbd4b9a91c31c7fb9541510028c5842ffa4eda
Ryan Dabler <rpdabler@gmail.com>
2018-03-18 15:28:55 -0600

    protect all "/api/journal" endpoints

--- src/routes/journals.js
+++ src/routes/journals.js
@@ -5,6 +5,7 @@
 ////////////////////////////
 const router        = require("express").Router();
 const jsonParser    = require("body-parser").json();
+const jwtAuth    = require("passport").authenticate("jwt", { session: false } );
 const { Journal }   = require("../models");
 const {
     checkRequiredFields,
@@ -15,36 +16,43 @@ const {
 ////////////////////////////
 // Routes
 ////////////////////////////
-router.get("/", (req, res) => {
-    Journal.find()
-           .then(journalEntries => {
-            res.json( { journal: journalEntries.map(entry => entry.serialize()) } );
-           })
-           .catch(err => {
-            console.error(err);
-            res.status(500).json( { message: "Internal server error" } );
-        });
+router.get(
+    "/",
+    jwtAuth,
+    (req, res) => {
+        Journal.find( { userId: req.user.id } )
+            .then(journalEntries => {
+                res.json( { journal: journalEntries.map(entry => entry.serialize()) } );
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json( { message: "Internal server error" } );
+            });
 });
 
-router.get("/:id", (req, res) => {
-    const { id } = req.params;
-    Journal.findById(id)
-           .then(journalEntry => {
-            res.json( { journal: journalEntry.serialize() } );
-           })
-           .catch(err => {
-            console.error(err);
-            res.status(500).json( { message: "Internal server error" } );
-        });
+router.get(
+    "/:id",
+    jwtAuth,
+    (req, res) => {
+        const { id } = req.params;
+        Journal.find( { _id: id, userId: req.user.id } )
+            .then(journalEntry => {
+                res.json( { journal: journalEntry.map(je => je.serialize()) } );
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json( { message: "Internal server error" } );
+            });
 });
 
 router.post(
     "/",
+    jwtAuth,
     jsonParser,
     checkRequiredFields(["date", "scope", "text"]),
     (req, res) => {
         const { date, scope, text } = req.body;
-        Journal.create( { date, scope, text } )
+        Journal.create( { date, scope, text, userId: req.user.id } )
                .then(journalEntry => res.status(201).json(journalEntry.serialize()))
                .catch(err => {
                    console.error(err);
@@ -53,24 +61,28 @@ router.post(
     }
 );
 
-router.delete("/:id", (req, res) => {
-    const { id } = req.params;
-    Journal.findByIdAndRemove(id)
-        .then(journalEntry => res.status(204).end())
-        .catch(err => {
-            console.error(err);
-            res.status(500).json( { message: "Internal server error" } );
-        });
+router.delete(
+    "/:id",
+    jwtAuth,
+    (req, res) => {
+        const { id } = req.params;
+        Journal.findOneAndRemove( {_id: id, userId: req.user.id } )
+            .then(journalEntry => res.status(204).end())
+            .catch(err => {
+                console.error(err);
+                res.status(500).json( { message: "Internal server error" } );
+            });
 });
 
 router.put(
     "/:id",
+    jwtAuth,
     jsonParser,
     validateIds,
     generateUpdateDocument(["date", "scope", "text"]),
     (req, res) => {
         const { id } = req.params;
-        Journal.findByIdAndUpdate(id, res.locals.updatedDoc)
+        Journal.findOneAndUpdate( { _id: id, userId: req.user.id }, res.locals.updatedDoc)
             .then(updatedJournalEntry => res.status(200).end())
             .catch(err => {
                 console.error(err);
