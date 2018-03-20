3ef1beace517f95bb05f69c143500caacf25befe
Ryan Dabler <rpdabler@gmail.com>
2018-03-20 14:20:21 -0600

    bring virtual properties into toJSON() for user model

--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true }
-});
+}, { toJSON: { virtuals: true } } );
 
 userSchema.virtual("crops", {
     ref: "Crop",
@@ -26,11 +26,15 @@ userSchema.methods.serialize = function() {
 }
 
 userSchema.methods.populatedSerialize = function() {
+    const user = this.toJSON();
+    const crops = this.crops.map(crop => {
+        return crop.serialize();
+    });
     return {
-        id: this._id,
-        username: this.username,
-        email: this.email,
-        crops: this.crops
+        id: user._id.toString(),
+        username: user.username,
+        email: user.email,
+        crops
     };
 }
 
