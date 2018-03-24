d6c2a25020dac2712cbb56ecbbbebace1ce73f16
Ryan Dabler <rpdabler@gmail.com>
2018-03-23 21:01:45 -0600

    add Authorization as allowed header

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ passport.use(jwtStrategy);
 // CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     next();
 });
